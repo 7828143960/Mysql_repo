174ef5378cd7f016bbc557330681b377072ea23b
Shreya Jaiswal <145312170+7828143960@users.noreply.github.com>
2023-12-16 16:02:29 +0530

    Update variable.tf

--- Mysql_Terraform/network/variable.tf
+++ Mysql_Terraform/network/variable.tf
@@ -1,33 +1,33 @@
 variable "vpc_name" {
-  type = string   
+  type = string
 }
 variable "tenancy" {
     type = string
 }
 variable "vpc_cidr" {
-  type = string  
+  type = string
 }
 variable "pub_subnet_names" {
-  type = list(string)  
+  type = list(string)
 }
 variable "pub_cidr" {
-  type = list(string)    
+  type = list(string)
 }
 variable "pvt_subnet_names" {
-  type = list(string)    
+  type = list(string)
 }
 variable "pvt_cidr" {
-  type = list(string)    
+  type = list(string)
 }
 variable "igw_name" {
-  type = string   
+  type = string
 }
 variable "nat_name" {
-  type = string   
+  type = string
 }
 variable "pub_rt_names" {
-  type = string   
+  type = string
 }
 variable "pvt_rt_names" {
-  type = string   
+  type = string
 }
