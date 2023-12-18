5401c2730122b41ac2f27e52bb1f8061a4eec58d
Shreya Jaiswal <145312170+7828143960@users.noreply.github.com>
2023-12-18 18:21:39 +0530

    Update variable.tf

--- Mysql_Terraform/security/variable.tf
+++ Mysql_Terraform/security/variable.tf
@@ -1,24 +1,95 @@
+variable "inbound_ports" {
+  description = "List of inbound ports and protocols"
+  type        = list(map(number))
+  default     = [
+    { port = 80, protocol = "tcp" },
+    { port = 22, protocol = "tcp" },
+    { port = 443, protocol = "tcp" },
+    { port = -1, protocol = "icmp" },
+  ]
+}
+
+variable "outbound_ports" {
+  description = "List of outbound ports"
+  type        = list(number)
+  default     = [0]
+}
+
 variable "bastion_instance_name" {
-  type = string 
+  description = "Name for the bastion EC2 instance"
+  type        = string
+  default     = "bastion-instance"
 }
+
+variable "associate_public_ip_address" {
+  description = "Associate a public IP address with the instance"
+  type        = bool
+  default     = true
+}
+
+variable "volume_size" {
+  description = "Root volume size for the instance"
+  type        = number
+  default     = 29
+}
+
 variable "pvt_instance_name" {
-  type = list(string)    
+  description = "List of names for private EC2 instances"
+  type        = list(string)
+  default     = ["mysql-master", "mysql-slave"]
 }
+
 variable "instance_type" {
-  type = string   
+  description = "Type of EC2 instance"
+  type        = string
+  default     = "t2.micro"
 }
+
+# Uncomment and customize if needed
+# variable "instance_tags" {
+#   description = "Tags for the private EC2 instances"
+#   type        = list(map(string))
+#   default     = [
+#     { Name = "example-instance", DB = "mysql" },
+#     # Add more tags as needed
+#   ]
+# }
+
 variable "sec_grp" {
-  type = string   
+  description = "ID of the security group for EC2 instances"
+  type        = string
+  default     = "mysql_sg"
 }
+
+variable "ingress_cidr_blocks" {
+  description = "CIDR blocks for inbound traffic"
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+}
+
+variable "egress_cidr_blocks" {
+  description = "CIDR blocks for outbound traffic"
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+}
+
 variable "key_name" {
-  type = string
+  description = "Name of the key pair for EC2 instances"
+  type        = string
+  default     = "mysql.pem"
 }
+
 variable "pub_sub_id" {
-  type = string
+  description = "ID of the public subnet for EC2 instances"
+  type        = string
 }
+
 variable "pvt_sub_id" {
-  type = list(string)
+  description = "List of IDs for private subnets for EC2 instances"
+  type        = list(string)
 }
+
 variable "vpc_id" {
-  type = string
+  description = "ID of the VPC for EC2 instances"
+  type        = string
 }
