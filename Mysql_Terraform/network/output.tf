20a27c2f6b3e8099383cf9a1784376e38c946fdd
Shreya Jaiswal <145312170+7828143960@users.noreply.github.com>
2023-12-18 18:19:25 +0530

    Update output.tf

--- Mysql_Terraform/network/output.tf
+++ Mysql_Terraform/network/output.tf
@@ -1,12 +1,19 @@
 output "vpc_id" {
-    value = aws_vpc.mysql_vpc.id
+  description = "The ID of the MySQL VPC"
+  value       = aws_vpc.mysql_vpc.id
 }
+
 output "vpc_id_print" {
-    value = aws_vpc.mysql_vpc[*].id
+  description = "A list of MySQL VPC IDs (printed)"
+  value       = aws_vpc.mysql_vpc[*].id
 }
+
 output "pub_subnet_id" {
-    value = aws_subnet.public[*].id
+  description = "A list of IDs for the public subnets"
+  value       = aws_subnet.public[*].id
 }
+
 output "pvt_subnet_id" {
-    value = aws_subnet.private[*].id
+  description = "A list of IDs for the private subnets"
+  value       = aws_subnet.private[*].id
 }
