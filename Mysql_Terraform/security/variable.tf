0067dd3df9c746fe81edc5c40c235e3081f65702
Shreya Jaiswal <145312170+7828143960@users.noreply.github.com>
2023-12-19 15:41:02 +0530

    Update variable.tf

--- Mysql_Terraform/security/variable.tf
+++ Mysql_Terraform/security/variable.tf
@@ -10,9 +10,11 @@ variable "inbound_ports" {
 }
 
 variable "outbound_ports" {
-  description = "List of outbound ports"
-  type        = list(number)
-  default     = [0]
+  description = "List of outbound ports and protocols"
+  type        = list(map(any))
+  default     = [
+    { port = 0, protocol = "-1" },
+  ]
 }
 
 variable "bastion_instance_name" {
